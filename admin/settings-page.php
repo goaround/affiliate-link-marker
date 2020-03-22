5c57a6c174a885f69c45e35eb8ce6229c4a0722e
Johannes Kinast <johannes.kinast@gmail.com>
2020-03-22 13:09:15 +0100

    0.1.0 Settings Page added and more

---
+++ admin/settings-page.php
@@ -0,0 +1,154 @@
+<?php
+namespace TD\Affiliate\Marker\Admin;
+
+use \TD\Affiliate\Marker\Links;
+
+class SettingsPage {
+	private string $option_group = 'affiliate-marker-settings';
+	private string $page = 'affiliate-marker';
+	private string $disclosure;
+	private array $domains;
+
+	public function __construct() {
+		add_action( 'admin_menu', [ $this, 'add_plugin_page' ] );
+		add_action( 'admin_init', [ $this, 'page_init' ] );
+	}
+
+	public function add_plugin_page() {
+		$this->disclosure = get_option( Links::$options_name_disclosure, __( '* What the star implies: Links marked with a * mean that we will receive a commission if a booking or a specific action is made via the linked provider. There will be no additional costs for you. Also, we won\'t receive any money just by setting links.', 'td-affiliate-marker' ) );
+		$this->domains = (array) get_option( Links::$options_name_domains, Links::$domains );
+
+		add_options_page(
+			'Affiliate Marker', // page_title
+			'Affiliate Marker', // menu_title
+			'manage_options', // capability
+			$this->page, // menu_slug
+			array( $this, 'create_admin_page' ) // function
+		);
+	}
+
+	public function create_admin_page() {
+			?>
+		<div class="wrap">
+			<h2>Affiliate Marker</h2>
+			<p></p>
+			<?php settings_errors(); ?>
+
+			<form method="post" action="options.php">
+				<?php
+					settings_fields( $this->option_group ); // option_group
+					do_settings_sections( $this->page ); // page
+					submit_button();
+				?>
+			</form>
+		</div>
+	<?php }
+
+	public function page_init() {
+		register_setting(
+			$this->option_group, // option_group
+			Links::$options_name_disclosure, // option_name
+			[
+				'type' => 'string',
+				'description' => 'Affiliate disclosure printed on the button of every post which includes at least one affiliate Link.',
+				'sanitize_callback' => [ $this, 'sanitize_disclosure' ],
+				'show_in_rest' => true,
+				'default' => __( '* What the star implies: Links marked with a * mean that we will receive a commission if a booking or a specific action is made via the linked provider. There will be no additional costs for you. Also, we won\'t receive any money just by setting links.', 'td-affiliate-marker' ),
+			],
+		);
+
+		register_setting(
+			$this->option_group, // option_group
+			Links::$options_name_domains, // option_name
+			[
+				'description' => 'List of affiliate domains',
+				'sanitize_callback' => [ $this, 'sanitize_domains' ],
+				'default' => \TD\Affiliate\Marker\Links::$domains,
+			],
+		);
+
+		add_settings_section(
+			'affiliate_marker_disclosure_section', // id
+			__( 'Affiliate Disclosure','td-affiliate-marker'), // title
+			[ $this, 'section_disclosure' ], // callback
+			$this->page // page
+		);
+
+		add_settings_section(
+			'affiliate_marker_domain_section', // id
+			__( 'Affiliate Domains','td-affiliate-marker'), // title
+			[ $this, 'section_domains' ], // callback
+			$this->page // page
+		);
+
+		add_settings_field(
+			Links::$options_name_disclosure, // id
+			__( 'Disclosure Text','td-affiliate-marker'), // title
+			[ $this, 'disclosure_callback' ], // callback
+			$this->page, // page
+			'affiliate_marker_disclosure_section', // section
+			[
+				'label_for' => Links::$options_name_disclosure,
+				'class' => 'large-text',
+			],
+		);
+
+		add_settings_field(
+			Links::$options_name_domains, // id
+			__( 'Domains','td-affiliate-marker'), // title
+			[ $this, 'domains_callback' ], // callback
+			$this->page, // page
+			'affiliate_marker_domain_section', // section
+			[
+				'label_for' => Links::$options_name_domains,
+				'class' => 'large-text',
+			],
+		);
+	}
+
+	public function sanitize_disclosure( $input ) {
+		return sanitize_text_field( $input );
+	}
+
+	public function sanitize_domains( $input ) {
+		// print_r($input);
+		// exit;
+		$domains_textarea = trim( esc_textarea( $input ) );
+		$domains = (array) explode( PHP_EOL, $domains_textarea );
+		$domains = array_map( 'trim', $domains );
+
+		return $domains;
+	}
+
+	public function section_disclosure() {
+		_e( 'The Disclosure, displayed at the end of each post which includes at least one Affiliate Link.','td-affiliate-marker');
+	}
+
+	public function section_domains() {
+		_e( 'List of Domains and URL parts to detect Affiliate Links in your content. One per line. Can include full Domains e.g. "amazon.com" or just parts of it e.g. "amazon"','td-affiliate-marker');
+	}
+
+	public function disclosure_callback( $args ) {
+		printf(
+			'<input class="%s" type="text" name="%s" id="affiliate_marker_disclosure" value="%s">',
+			$args['class'] ?? '',
+			$args['label_for'] ?? '',
+			$this->disclosure,
+		);
+	}
+
+	public function domains_callback( $args ) {
+
+		$domains = array_map( 'esc_textarea', $this->domains );
+
+		printf(
+			'<textarea class="%s" rows="30" name="%s" id="affiliate_marker_domains">%s</textarea>',
+			$args['class'] ?? '',
+			$args['label_for'] ?? '',
+			implode( PHP_EOL, $domains ),
+		);
+	}
+
+}
+
+$affiliate_marker_settings_page = new SettingsPage();
--- affiliate-marker.php
+++ affiliate-marker.php
@@ -1,18 +1,18 @@
 <?php
+namespace TD\Affiliate\Marker;
 /**
- * Plugin Name:  Affiliate Marker
+ * Plugin Name:  Affiliate Link Marker
  * Plugin URI:
- * Description: Marks Affiliate Links
+ * Description: Marks Affiliate Links with a * and adds a disclosure at the end of the post
  * Author:      Johannes Kinast <johannes@travel-dealz.de>
  * Author URI:  https://go-around.de
- * Version:     0.0.2
+ * Version:     0.1.0
  * Text Domain: td-affiliate-marker
  * Domain Path: /languages
  */
-namespace TD\Affiliate\Marker;
 
 class Links {
-	public $domains = [
+	public static $domains = [
 		'/go/',
 		'partners.webmasterplan.com',
 		'track.webgains.com',
@@ -51,16 +51,12 @@ class Links {
 		'airportag.com',
 	];
 
+	public static string $options_name_disclosure = 'affiliate_marker_disclosure';
+	public static string $options_name_domains = 'affiliate_marker_domains';
+
 	public $domain_search = '';
 
 	public $found_match = false;
-
-	public function __construct() {
-		$domains = array_map( function( $value ) {
-			return preg_quote( $value, '/' );
-		}, $this->domains );
-		$this->domain_search = implode( '|', $domains );
-	}
 
 	public function nofollow_content_links( $content ) {
 
@@ -69,12 +65,29 @@ class Links {
 			[$this, 'check_match'],
 			$content
 		);
-		//exit;
+
 		return $content;
 	}
 
+	public function domain_search_regex() {
+
+		if ( ! empty( $this->domain_search ) ) {
+			return $this->domain_search;
+		}
+
+		$domains = get_option( Links::$options_name_domains, Links::$domains );
+
+		$domains = array_map( function( $value ) {
+			return preg_quote( $value, '/' );
+		}, $domains );
+
+		$this->domain_search = implode( '|', $domains );
+
+		return $this->domain_search;
+	}
+
 	private function check_match( $match ) {
-		if ( preg_match( '/.*' . $this->domain_search. '.*/', $match[1] ) ) {
+		if ( ! empty( $this->domain_search_regex() ) && preg_match( '/.*' . $this->domain_search_regex() . '.*/', $match[1] ) ) {
 			// If nofollow already set
 			if ( 0 < strpos( $match[0], 'nofollow external' ) ) {
 				return $match[0];
@@ -92,34 +105,38 @@ class Links {
 
 	public function rel_nofollow( $rel, $link_html ) {
 
-		if ( preg_match( '/.*' . $this->domain_search. '.*/', $link_html ) ) {
+		if ( preg_match( '/.*' . $this->domain_search_regex() . '.*/', $link_html ) ) {
 			return 'nofollow sponsored noopener';
 		}
 
 		return $rel;
 	}
 
-	public function add_notice( $content ) {
-		if ( strpos( $content, 'sponsored' ) ) {
-			$content .= '<p><aside>' . __( '* What the star implies: Links marked with a * mean that we will receive a commission if a booking or a specific action is made via the linked provider. There will be no additional costs for you, and in return, we can offer you Travel-Dealz without annoying display advertisement. Also, we won\'t receive any money just by setting links.', 'td-affiliate-marker' ) . '</aside></p>';
-		}
-		return $content;
-	}
-
-	public static function style_marker() {
-		echo '<style>a[rel*=sponsored]{position:relative}a[rel*=sponsored]:after{text-decoration:none;font-weight:400;display:inline-block;content:"*"}</style>';
-	}
-
 }
-$Nofollow = new Links;
+$AffiliateLinks = new Links;
 
-add_filter( 'the_content', [ $Nofollow, 'nofollow_content_links' ], 11 );
-add_filter( 'the_content', [ $Nofollow, 'add_notice' ], 20 );
-add_filter( 'wp_targeted_link_rel', [ $Nofollow, 'rel_nofollow' ], 10, 2 );
-add_action( 'wp_head', 'TD\Affiliate\Marker\Links::style_marker' );
+add_filter( 'the_content', [ $AffiliateLinks, 'nofollow_content_links' ], 11 );
+add_filter( 'wp_targeted_link_rel', [ $AffiliateLinks, 'rel_nofollow' ], 10, 2 );
 
+function add_notice( $content ) {
+	if ( strpos( $content, 'sponsored' ) ) {
+		$disclosure = get_option( Links::$options_name_disclosure, __( '* What the star implies: Links marked with a * mean that we will receive a commission if a booking or a specific action is made via the linked provider. There will be no additional costs for you. Also, we won\'t receive any money just by setting links.','td-affiliate-marker') );
+		$content .= '<p><aside>' . $disclosure . '</aside></p>';
+	}
+	return $content;
+}
+add_filter( 'the_content', __NAMESPACE__ . '\add_notice', 20 );
+
+function style() {
+	echo '<style>a[rel*=sponsored]{position:relative}a[rel*=sponsored]:after{text-decoration:none;font-weight:400;display:inline-block;content:"*"}</style>';
+}
+add_action( 'wp_head', __NAMESPACE__ . '\style' );
+
+if ( is_admin() ) {
+	require_once __DIR__ . '/admin/settings-page.php';
+}
 
 function load_plugin_textdomain() {
-    \load_plugin_textdomain( 'td-affiliate-marker', FALSE, basename( dirname( __FILE__ ) ) . '/languages/' );
+    \load_plugin_textdomain( td-affiliate-marker, FALSE, basename( dirname( __FILE__ ) ) . '/languages/' );
 }
 add_action( 'plugins_loaded', 'TD\Affiliate\Marker\load_plugin_textdomain' );
